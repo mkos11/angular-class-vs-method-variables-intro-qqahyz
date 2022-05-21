14a178045e9faf989621353d04651003c4300b22
mkos11 <mrunaljain1102@gmail.com>
2022-05-21 13:10:25 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-
-export class AppComponent  {
- 
+export class AppComponent {
   // variables declared here will be class variables
+  prefix = 'i am';
+  name = 'best person';
 
   speak() {
     // variables declared here will be method variables
+    const sentence = this.prefix + this.name;
+    return sentence;
   }
 }
